c761f6ea68a3eb117055b6d3280edf8ab720faab
nizar202108010373 <nizaralamsyah21@gmail.com>
2022-10-20 11:41:19 +0700

    typedata

---
+++ typedata.py
@@ -0,0 +1,52 @@
+# type data di python
+
+#type data integer
+alva=12
+print (alva)
+print (type(alva))
+
+#type data float
+bepa=12.5
+print (bepa)
+print (type(bepa))
+
+#type data complex
+bahasa=12j
+print(bahasa)
+print(type(bahasa))
+
+#type data string
+A="hello world"
+print (A)
+print(type(A))
+
+#type data tuple
+B=("A","B","C")
+C=12,5,3,1
+print(B,C)
+print(type(B),type(C))
+
+#type data lis
+D=["A","B","C"]
+print(D)
+print(type(D))
+
+a=1
+a=2
+print(a)
+
+#type data set
+D={"saya", "sedang", "kuliah"}
+print(D)
+print(type(D))
+
+#type data dictonary
+D={"nama": "junius", "umur":20}
+print(D)
+print(type(D))
+
+#type data bool
+a=True
+b=False
+print(a,b)
+print(type(a),type(b))
